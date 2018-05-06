99f7ca2f3a496b214cadbbe0f4544558eb92dec6
Akai Genki <akai@ei.sanken.osaka-u.ac.jp>
2018-05-06 18:39:22 +0900

    [update] 関数findPatternにおいて画像の拡大・縮小・回転を考慮するように実装
    画像の回転角，拡大率を全探索する（探索範囲はgiven）実装になっている．
    ただ，この実装だと探索にものすごく時間がかかる（数時間くらい）ので要改善．
    精度に関してもそこまで良くなさそうなのでそれも要改善．

--- src/findit.c
+++ src/findit.c
@@ -39,69 +39,84 @@ double *scaling ;
 	int xx, yy, dx, dy ;
 	int x0, y0, x1, y1 ;
 	int diff, pels, dr, dg, db ;
+	int deg, sc;
 	RGB_PACKED_PIXEL *pixel ;
-
-	/*
-	 *  テンプレートの中心から見た, テンプレートの左上と右下の座標
-	 *  (x0,y0) と (x1, y1) をあらかじめ求めておく.
-	 */
-	x0 = -( template->cols / 2 ) ;
-	y0 = -( template->rows / 2 ) ;
-	x1 = ( template->cols - 1 ) / 2 ;
-	y1 = ( template->rows - 1 ) / 2 ;
+	RGB_PACKED_IMAGE *target ;
 
 	/*
 	 *  テンプレートを当てはめる位置を探索画像の全範囲に移動させながら,
-	 *  テンプレートと探索画像の差がもっとも小さい位置 (posx, posy) を
+	 *  テンプレートと探索画像の差がもっとも小さいパラメータ (posx, posy, rot, scal) を
 	 *  見付ける.
 	 */
 	mindiff = 0x7fffffff ;
 	posx = 0; posy = 0;
-	for ( yy = -y1 ; yy < image->rows - y0 ; yy++ ) {
-		for ( xx = -x1 ; xx < image->cols - x0 ; xx++ ) {
+	rot = 0; scal = 10;
+
+	for (deg = -30; deg <= 30; deg++) {
+		for (sc = 5; sc <= 20; sc++) {
+			target = affine(template, (double)deg, (double)sc / 10);
 
 			/*
-			 *  ある位置 (xx, yy) におけるふたつの画像間の差を求める.
-			 *  画像の差とは, ここでは R,G,B 値の差の絶対値の累積を用いた.
-			 */
-			diff = 0 ; /* R,G,B それぞれの画素の差の累計 */
-			pels = 0 ; /* 有効な比較を行った画素数 */
-			pixel = template->data_p ; /* テンプレートデータの先頭画素 */
-			for ( dy = yy + y0 ; dy <= yy + y1 ; dy++ ) {
-				if ( dy >= 0 && dy < image->rows ) {
-					/* 探索画像の外(上/下)に出てないことを確認し... */
-					for ( dx = xx + x0 ; dx <= xx + x1 ; dx++, pixel++ ) {
-						if ( dx >= 0 && dx < image->cols ) {
-							/* 探索画像の外(左/右)に出てないことを確認し... */
-							if ( pixel->r != 255 || pixel->g != 255 || pixel->b != 255 ) {
-								/* テンプレート画素が背景でないことを確認し... */
-								pels ++ ;
-								if (( dr = image->p[dy][dx].r - pixel->r ) < 0 ) dr = -dr ;
-								if (( dg = image->p[dy][dx].g - pixel->g ) < 0 ) dg = -dg ;
-								if (( db = image->p[dy][dx].b - pixel->b ) < 0 ) db = -db ;
-								diff += ( dr + dg + db ) ;
+			*  テンプレートの中心から見た, テンプレートの左上と右下の座標
+			*  (x0,y0) と (x1, y1) をあらかじめ求めておく.
+			*/
+			x0 = -( target->cols / 2 ) ;
+			y0 = -( target->rows / 2 ) ;
+			x1 = ( target->cols - 1 ) / 2 ;
+			y1 = ( target->rows - 1 ) / 2 ;
+
+			for ( yy = -y1 ; yy < image->rows - y0 ; yy++ ) {
+				for ( xx = -x1 ; xx < image->cols - x0 ; xx++ ) {
+
+					/*
+					*  ある位置 (xx, yy) におけるふたつの画像間の差を求める.
+					*  画像の差とは, ここでは R,G,B 値の差の絶対値の累積を用いた.
+					*/
+					diff = 0 ; /* R,G,B それぞれの画素の差の累計 */
+					pels = 0 ; /* 有効な比較を行った画素数 */
+					pixel = target->data_p ; /* テンプレートデータの先頭画素 */
+					for ( dy = yy + y0 ; dy <= yy + y1 ; dy++ ) {
+						if ( dy >= 0 && dy < image->rows ) {
+							/* 探索画像の外(上/下)に出てないことを確認し... */
+							for ( dx = xx + x0 ; dx <= xx + x1 ; dx++, pixel++ ) {
+								if ( dx >= 0 && dx < image->cols ) {
+									/* 探索画像の外(左/右)に出てないことを確認し... */
+									if ( pixel->r != 255 || pixel->g != 255 || pixel->b != 255 ) {
+										/* テンプレート画素が背景でないことを確認し... */
+										pels ++ ;
+										if (( dr = image->p[dy][dx].r - pixel->r ) < 0 ) dr = -dr ;
+										if (( dg = image->p[dy][dx].g - pixel->g ) < 0 ) dg = -dg ;
+										if (( db = image->p[dy][dx].b - pixel->b ) < 0 ) db = -db ;
+										diff += ( dr + dg + db ) ;
+									}
+								}
 							}
+						}
+						else {
+							/* テンプレートを一行読み飛ばしたので, ポインタを調整する */
+							pixel += target->cols ;
+						}
+					}
+					if ( pels ) { /* 有効に差が累積されていた場合には... */
+						diff /= pels ; /* 画素の差の累計を有効画素数で割って正規化する */
+						/*
+						*  これまでの結果と比較し, 差が小さければその位置を採用する.
+						*/
+						if ( diff < mindiff ) {
+							mindiff = diff ;
+							posx = xx ;
+							posy = yy ;
+							rot = deg;
+							scal = sc;
 						}
 					}
 				}
-				else {
-					/* テンプレートを一行読み飛ばしたので, ポインタを調整する */
-					pixel += template->cols ;
-				}
 			}
-			if ( pels ) { /* 有効に差が累積されていた場合には... */
-				diff /= pels ; /* 画素の差の累計を有効画素数で割って正規化する */
-				/*
-				 *  これまでの結果と比較し, 差が小さければその位置を採用する.
-				 */
-				if ( diff < mindiff ) {
-					mindiff = diff ;
-					posx = xx ;
-					posy = yy ;
-				}
-			}
+
+			freeRGBPackedImage( target );
 		}
 	}
+
 	if ( mindiff == 0x7fffffff )
 		return ( HAS_ERROR ) ; /* 画像間の差が更新されていないので失敗と判断 */
 
@@ -110,7 +125,7 @@ double *scaling ;
 	 */
 	*cx = (double)posx ;
 	*cy = (double)posy ;
-	*rotation = 0.0 ;
-	*scaling = 1.0 ;
+	*rotation = (double)rot ;
+	*scaling = (double)scal / 10;
 	return ( NO_ERROR ) ;
 }
